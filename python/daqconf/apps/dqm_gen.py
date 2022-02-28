70cbaf60948057d7b5fb79d2f4dace612e3f541c
jmcarcell <jmcarcell@users.noreply.github.com>
2022-02-28 16:19:55 +0100

    Add a number of frames

--- python/daqconf/apps/dqm_gen.py
+++ python/daqconf/apps/dqm_gen.py
@@ -53,7 +53,7 @@ def get_dqm_app(RU_CONFIG=[],
                  MODE="readout",
                  DF_RATE=10,
                  DF_ALGS='hist mean_rms fourier_sum',
-                 DF_TIME_WINDOW=trigger_window_before_ticks + trigger_window_after_ticks,
+                 DF_TIME_WINDOW=0,
                  DEBUG=False,
                  ):
 
@@ -138,7 +138,7 @@ def get_dqm_app(RU_CONFIG=[],
                               df_seconds=DF_RATE * NUM_DF_APPS if MODE == 'df' else 0,
                               df_offset=DF_RATE * DQMIDX,
                               df_algs=algs_bitfield,
-                              df_frames=DF_TIME_WINDOW / 25,
+                              df_num_frames=DF_TIME_WINDOW / 25,
                           )
                           )
                           ]
