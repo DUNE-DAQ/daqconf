7820e58683573b066c3475e94209d66d8321f195
jmcarcell <jmcarcell@users.noreply.github.com>
2022-02-28 14:34:38 +0100

    Change name

--- python/daqconf/apps/dqm_gen.py
+++ python/daqconf/apps/dqm_gen.py
@@ -136,7 +136,7 @@ def get_dqm_app(RU_CONFIG=[],
                               readout_window_offset=10**7 / DATA_RATE_SLOWDOWN_FACTOR, # 10^7 works fine for WIBs with no slowdown
                               df_seconds=DF_RATE * NUM_DF_APPS if MODE == 'df' else 0,
                               df_offset=DF_RATE * DQMIDX,
-                              df_algs_bitfield=algs_bitfield
+                              df_algs=algs_bitfield
                           )
                           )
                           ]
