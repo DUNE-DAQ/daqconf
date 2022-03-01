283caa81a6f2dc172f038b7575a82d2728659d42
Eric Flumerfelt <eflumerf@fnal.gov>
2022-03-01 11:10:52 -0600

    At Pierre's suggestion, move the metadata generation to a separate "core" script, and call it from the _gen scripts. Remove the glob in favor of direct path manipulation, since we no longer have to support "python -m" execution.

---
+++ python/daqconf/core/metadata.py
@@ -0,0 +1,35 @@
+import json
+import os
+import sys
+from rich.console import Console
+from os.path import exists, join
+
+CLOCK_SPEED_HZ = 50000000
+
+# Add -h as default help option
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+console = Console()
+
+def write_metadata_file(json_dir, generator):
+    console.log("Generating metadata file")
+    with open(join(json_dir, f"{generator}.info"), 'w') as f:
+        daqconf_dir = os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0])))
+
+        buildinfo_file=join(daqconf_dir, "daqconf_build_info.json")
+        buildinfo = {}
+        #console.log(f"Buildinfo file is {buildinfo_file}")
+        if exists(buildinfo_file):
+            with open(buildinfo_file, 'r') as ff:
+                try:
+                    #console.log(f"Reading buildinfo file {buildinfo_file}")
+                    buildinfo = json.load(ff)
+                except json.decoder.JSONDecodeError as e:
+                    console.log(f"Error reading buildinfo file {buildinfo_file}: {e}")
+
+        daqconf_info = {
+            "command_line": ' '.join(sys.argv),
+            "daqconf_dir": daqconf_dir,
+            "build_info": buildinfo
+        }
+        json.dump(daqconf_info, f, indent=4, sort_keys=True)
