236e160d2ef954e7b062ebb53c937178c8995760
Philip Rodrigues <rodriges@fnal.gov>
2022-04-21 11:24:58 +0200

    Add and connect up TC buffer

--- python/daqconf/apps/trigger_gen.py
+++ python/daqconf/apps/trigger_gen.py
@@ -35,6 +35,8 @@ from appfwk.daqmodule import DAQModule
 from appfwk.conf_utils import Direction, Connection
 
 TA_ELEMENT_ID = 10_000
+TC_REGION_ID = 20_000
+TC_ELEMENT_ID = 0
 
 #FIXME maybe one day, triggeralgs will define schemas... for now allow a dictionary of 4byte int, 4byte floats, and strings
 moo.otypes.make_type(schema='number', dtype='i4', name='temp_integer', path='temptypes')
@@ -89,10 +91,25 @@ def get_trigger_app(SOFTWARE_TPG_ENABLED: bool = False,
         
         modules += [DAQModule(name = 'tcm',
                               plugin = 'TriggerCandidateMaker',
-                              connections = {#'input' : Connection(f'tcm.taset_q'),
-                                  'output': Connection(f'mlt.trigger_candidate_source')},
-                              conf = config_tcm)]
+                              connections = {},
+                                  # 'output': Connection(f'mlt.trigger_candidate_source')},
+                              conf = config_tcm),
 
+                    DAQModule(name = 'tc_buf',
+                              plugin = 'TCBuffer',
+                              connections = {},
+                              conf = bufferconf.Conf(latencybufferconf = readoutconf.LatencyBufferConf(latency_buffer_size = 100_000,
+                                                                                                       region_id = TC_REGION_ID,
+                                                                                                       element_id = TA_ELEMENT_ID),
+                                                     requesthandlerconf = readoutconf.RequestHandlerConf(latency_buffer_size = 100_000,
+                                                                                                         pop_limit_pct = 0.8,
+                                                                                                         pop_size_pct = 0.1,
+                                                                                                         region_id = TC_REGION_ID,
+                                                                                                         element_id = TC_ELEMENT_ID,
+                                                                                                         # output_file = f"output_{idx + MIN_LINK}.out",
+                                                                                                         stream_buffer_size = 8388608,
+                                                                                                         retry_count = 1000,
+                                                                                                         enable_raw_recording = False)))]
 
         # Make one heartbeatmaker per link
         for ruidx, ru_config in enumerate(RU_CONFIG):
@@ -149,6 +166,7 @@ def get_trigger_app(SOFTWARE_TPG_ENABLED: bool = False,
                                                                                                                  element_id = TA_ELEMENT_ID,
                                                                                                                  # output_file = f"output_{idx + MIN_LINK}.out",
                                                                                                                  stream_buffer_size = 8388608,
+                                                                                                                 retry_count = 1000,
                                                                                                                  enable_raw_recording = False)))]
 
             for idy in range(RU_CONFIG[ru]["channel_count"]):
@@ -172,7 +190,7 @@ def get_trigger_app(SOFTWARE_TPG_ENABLED: bool = False,
 
     modules += [DAQModule(name = 'ttcm',
                           plugin = 'TimingTriggerCandidateMaker',
-                          connections={"output": Connection("mlt.trigger_candidate_source")},
+                          connections={}, #{"output": Connection("mlt.trigger_candidate_source")},
                           conf=ttcm.Conf(s1=ttcm.map_t(signal_type=TTCM_S1,
                                                        time_before=TRIGGER_WINDOW_BEFORE_TICKS,
                                                        time_after=TRIGGER_WINDOW_AFTER_TICKS),
@@ -219,9 +237,17 @@ def get_trigger_app(SOFTWARE_TPG_ENABLED: bool = False,
             mgraph.add_fragment_producer(region=region_id, element=TA_ELEMENT_ID, system="DataSelection",
                                          requests_in=f"{buf_name}.data_request_source",
                                          fragments_out=f"{buf_name}.fragment_sink")
-            mgraph.add_endpoint(f"tasets_from_maker_region_{region_id}", f"tam_{region_id}.output", Direction.OUT)
-            mgraph.add_endpoint(f"tasets_into_chain_region_{region_id}", f"tcm.input", Direction.IN)
+            mgraph.add_endpoint(f"tasets_from_maker_region_{region_id}",  f"tam_{region_id}.output",  Direction.OUT)
+            mgraph.add_endpoint(f"tasets_into_chain_region_{region_id}",  f"tcm.input",               Direction.IN)
             mgraph.add_endpoint(f"tasets_into_buffer_region_{region_id}", f"{buf_name}.taset_source", Direction.IN)
+
+        mgraph.add_endpoint(f"tcs_from_chain",  "tcm.output",                   Direction.OUT)
+        mgraph.add_endpoint(f"tcs_from_timing", "ttcm.output",                  Direction.OUT)
+        mgraph.add_endpoint(f"tcs_into_mlt",    "mlt.trigger_candidate_source", Direction.IN)
+        mgraph.add_endpoint(f"tcs_into_buffer", "tc_buf.tc_source",             Direction.IN)
+        mgraph.add_fragment_producer(region=TC_REGION_ID, element=TC_ELEMENT_ID, system="DataSelection",
+                                     requests_in="tc_buf.data_request_source",
+                                     fragments_out="tc_buf.fragment_sink")
 
 
     trigger_app = App(modulegraph=mgraph, host=HOST, name='TriggerApp')
