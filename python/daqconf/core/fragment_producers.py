5c0240f349a7b4fdec3bfcd0790726d75d96d739
Kurt Biery <biery@fnal.gov>
2022-07-08 15:15:33 -0500

    first-draft changes for correlated timeouts

--- python/daqconf/core/fragment_producers.py
+++ python/daqconf/core/fragment_producers.py
@@ -146,6 +146,7 @@ def connect_fragment_producers(app_name, the_system, verbose=False):
         df_mgraph.reset_module_conf("trb", trb.ConfParams(general_queue_timeout=old_trb_conf.general_queue_timeout,
                                                           reply_connection_name = fragment_connection_name,
                                                           max_time_window = old_trb_conf.max_time_window,
+                                                          trigger_record_timeout_ms = old_trb_conf.trigger_record_timeout_ms,
                                                           map=trb.mapgeoidconnections(new_trb_map)))
                           
     dqm_apps = [ (name,app) for (name,app) in the_system.apps.items() if re.match("dqm\d+_ru", name) ]
@@ -164,6 +165,7 @@ def connect_fragment_producers(app_name, the_system, verbose=False):
         dqm_mgraph.reset_module_conf("trb_dqm", trb.ConfParams(general_queue_timeout=old_trb_conf.general_queue_timeout,
                                                           reply_connection_name = fragment_connection_name,
                                                           max_time_window = old_trb_conf.max_time_window,
+                                                          trigger_record_timeout_ms = old_trb_conf.trigger_record_timeout_ms,
                                                           map=trb.mapgeoidconnections(new_trb_map)))
 
 def connect_all_fragment_producers(the_system, dataflow_name="dataflow", verbose=False):
