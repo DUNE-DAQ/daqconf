22f09536fe6c178df8d1e6d93a04372da86684bf
Charlie Batchelor <cbatchel@dunegpvm14.fnal.gov>
2023-02-15 10:43:26 -0600

    Add functionality to ignore known noisy channels.

--- python/daqconf/apps/trigger_gen.py
+++ python/daqconf/apps/trigger_gen.py
@@ -163,7 +163,7 @@ def get_trigger_app(CLOCK_SPEED_HZ: int = 50_000_000,
                                       plugin = 'TPChannelFilter',
                                       conf = chfilter.Conf(channel_map_name=CHANNEL_MAP_NAME,
                                                            keep_collection=True,
-                                                           keep_induction=False
+                                                           keep_induction=False,
 							   noisy_channels=NOISY_CHANNELS))]
             modules += [DAQModule(name = f'tpsettee_{link_id}',
                                   plugin = 'TPSetTee'),
