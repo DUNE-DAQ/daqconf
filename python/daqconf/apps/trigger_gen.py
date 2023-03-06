500b519d2eea872593c748c8947f6cb14929224d
Giovanna Lehmann Miotto <glehmann@np04-srv-024.cern.ch>
2023-03-06 16:00:51 +0100

    use . nomenclature for endpoint names

--- python/daqconf/apps/trigger_gen.py
+++ python/daqconf/apps/trigger_gen.py
@@ -289,6 +289,7 @@ def get_trigger_app(CLOCK_SPEED_HZ: int = 50_000_000,
         mgraph.connect_modules("ttcm.output",         "tctee_ttcm.input",             "TriggerCandidate", "ttcm_input", size_hint=1000)
         mgraph.connect_modules("tctee_ttcm.output1",  "mlt.trigger_candidate_input", "TriggerCandidate","tcs_to_mlt", size_hint=1000)
         mgraph.connect_modules("tctee_ttcm.output2",  "tc_buf.tc_source",             "TriggerCandidate","tcs_to_buf", size_hint=1000)
+        mgraph.add_endpoint("hsievents", "ttcm.hsi_input", "HSIEvent", Direction.IN)
 
     if len(TP_SOURCE_IDS) > 0:
         mgraph.connect_modules("tazipper.output", "tcm.input", data_type="TASet", size_hint=1000)
@@ -318,11 +319,8 @@ def get_trigger_app(CLOCK_SPEED_HZ: int = 50_000_000,
             mgraph.connect_modules(f'tasettee_region_{region_id}.output1', f'tazipper.input', queue_name="tas_to_tazipper",     data_type="TASet", size_hint=1000)
             mgraph.connect_modules(f'tasettee_region_{region_id}.output2', f'ta_buf_region_{region_id}.taset_source',data_type="TASet", size_hint=1000)
 
-    if USE_HSI_INPUT:
-        mgraph.add_endpoint("hsievents", "hsi_input", "HSIEvent", Direction.IN)
-        
-    mgraph.add_endpoint("td_to_dfo", "td_output", "TriggerDecision", Direction.OUT, toposort=True)
-    mgraph.add_endpoint("df_busy_signal", "dfo_inhibit_input", "TriggerInhibit", Direction.IN)
+    mgraph.add_endpoint("td_to_dfo", "mlt.td_output", "TriggerDecision", Direction.OUT, toposort=True)
+    mgraph.add_endpoint("df_busy_signal", "mlt.dfo_inhibit_input", "TriggerInhibit", Direction.IN)
 
     mgraph.add_fragment_producer(id=TC_SOURCE_ID["source_id"], subsystem="Trigger",
                                  requests_in="tc_buf.data_request_source",
