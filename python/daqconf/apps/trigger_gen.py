b3992b367d94af66c0cb56d5a3981d462ea76d6a
Kurt Biery <biery@fnal.gov>
2023-05-27 21:46:49 -0500

    Updated the tpset receiver connection name in the trigger

--- python/daqconf/apps/trigger_gen.py
+++ python/daqconf/apps/trigger_gen.py
@@ -355,8 +355,7 @@ def get_trigger_app(CLOCK_SPEED_HZ: int = 62_500_000,
     if len(TP_SOURCE_IDS) > 0:
         for tp_sid,tp_conf in TP_SOURCE_IDS.items():
                 # 1 buffer per link
-                host_underscore = tp_conf.host.replace('-','_')
-                link_id1=f"ru{host_underscore}_{tp_conf.card}_link{tp_conf.dro_source_id}"
+                link_id1=f"{tp_conf.dro_app_name}_link{tp_conf.dro_source_id}"
                 link_id2=f"tplink{tp_sid}"
                 buf_name=f'buf_{link_id2}'
 
