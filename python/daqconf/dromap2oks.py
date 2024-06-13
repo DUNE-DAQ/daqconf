75da3467ce67b25de8a2f2bdbe6e630b90b91951
Gordon Crone <g.crone@ucl.ac.uk>
2024-06-13 11:50:05 +0100

    Update for new schema with DetectorToDaqConnection

--- python/daqconf/dromap2oks.py
+++ python/daqconf/dromap2oks.py
@@ -27,17 +27,22 @@ def dro_json_to_oks(jsonfile, oksfile, source_id_offset, nomap, lcores):
     db = conffwk.Configuration("oksconflibs")
     db.create_db(oksfile, schemafiles)
 
-    groups = []
     eth_streams = []
+    hermes_streams = []
     flx_streams = []
+    eth_senders = []
+    flx_senders = []
+    links = []
     last_eth_pars = None
     last_felix_pars = None
     last_hermes_id = None
     eth_streams_found = False
     flx_streams_found = False
-    nic_dals = {}
-    stream_dals = {}
+
     rx_queue = 0
+    link_number = 0
+    last_tx_mac = None
+    last_tx_host = None
     for entry in jsonmap:
         source_id = entry["src_id"] + source_id_offset
         geo_id = entry["geo_id"]
@@ -49,57 +54,128 @@ def dro_json_to_oks(jsonfile, oksfile, source_id_offset, nomap, lcores):
                             )
         db.update_dal(geo_dal)
 
+        stream_dal = dal.DetectorStream(f"stream-{source_id}",
+                                        source_id = source_id,
+                                        geo_id = geo_dal
+                                        )
+        db.update_dal(stream_dal)
+
         if entry["kind"] == "eth":
             eth_source_id = source_id
             if not eth_streams_found:
                 eth_streams_found = True
-                nic_config_dal = dal.NICInterfaceConfiguration(
-                    f"nicConfig-{group_name}"
+                lcore_dal = dal.ProcessingResource(
+                    f"lcores-{group_name}",
+                    cpu_cores = lcores.split(',')
+                )
+                db.update_dal(lcore_dal)
+                nic_config_dal = dal.DPDKPortConfiguration(
+                    f"nicConfig-{group_name}",
+                    used_lcores = [ lcore_dal ]
                 )
                 db.update_dal(nic_config_dal)
+                address_table_dal = dal.IpbusAddressTable("Hermes-addrtab")
+                db.update_dal(address_table_dal)
+
+
             pars = entry["parameters"]
+            if last_eth_pars == None or pars["rx_mac"] != last_eth_pars["rx_mac"]:
+                nic_name = f"nic-{pars['rx_host']}"
+                print(f"New nic adding nic {pars['rx_mac']} with id {nic_name}")
+                rxnic_dal = dal.NICInterface(
+                    nic_name,
+                    mac_address = pars["rx_mac"],
+                    ip_address = pars["rx_ip"]
+                )
+                db.update_dal(rxnic_dal)
+
+                dpdkrec_dal = dal.DPDKReceiver(
+                    f"{pars['rx_host']}-receiver",
+                    uses = rxnic_dal,
+                    configuration = nic_config_dal
+                )
+                db.update_dal(dpdkrec_dal)
+
+
+
+            if last_tx_mac != None and pars["tx_mac"] != last_tx_mac:
+                link_dal = dal.HermesDataSender(
+                    hermes_link_id,
+                    link_id = link_number,
+                    contains = hermes_streams,
+                    uses = txnic_dal
+                )
+                db.update_dal(link_dal)
+                links.append(link_dal)
+                link_number = link_number + 1
+                eth_senders.append(link_dal)
+                hermes_streams = []
+
+            if last_tx_mac == None or pars["tx_mac"] != last_tx_mac:
+                if last_tx_host != pars['tx_host']:
+                    nic_num = -1
+                nic_num += 1
+                nic_name = f"nic-{pars['tx_host']}-{nic_num}"
+                print(f"Adding NIC {nic_name}")
+                txnic_dal = dal.NICInterface(
+                    nic_name,
+                    mac_address = pars["tx_mac"],
+                    ip_address = pars["tx_ip"]
+                )
+                db.update_dal(txnic_dal)
+
             if last_eth_pars != None:
+                if pars["tx_host"] != last_eth_pars['tx_host']:
+                    # print(f"Adding HermesController {hermes_id} for {pars['tx_host']=} {last_pars['tx_host']=}")
+                    hermes_controller_dal = dal.HermesController(
+                        hermes_id,
+                        uri = f"ipbusudp-2.0://{last_eth_pars['tx_host']}:50001",
+                        address_table = address_table_dal,
+                        links = links,
+                        destination = rxnic_dal
+                    )
+                    db.update_dal(hermes_controller_dal)
+                    links = []
+                    link_number = 0
+
                 #print(f"streams in nic {pars['rx_mac']} = {len(streams)}")
                 if pars["rx_mac"] != last_eth_pars["rx_mac"]:
-                    nic_name = f"nic-{last_eth_pars['rx_host']}"
-                    print(f"New nic adding nic {last_eth_pars['rx_mac']} with id {nic_name}")
-                    nic_dal = dal.NICInterface(
-                        nic_name,
-                        rx_hostname=last_eth_pars["rx_host"],
-                        rx_mac=last_eth_pars["rx_mac"],
-                        rx_ip=last_eth_pars["rx_ip"],
-                        rx_iface=last_eth_pars["rx_iface"],
-                        rx_pcie_addr=last_eth_pars["rx_pcie_dev"],
-                        contains=eth_streams,
-                        configuration=nic_config_dal
+                    rset_dal = dal.ResourceSetAND(
+                        f"{last_eth_pars['rx_host']}-senders",
+                        contains = eth_senders
                     )
-                    db.update_dal(nic_dal)
-                    nic_dals[nic_name] = nic_dal
-                    rogroup_dal = dal.ReadoutGroup(
-                        f"group-{last_eth_source_id}",
-                        contains=[nic_dal]
+                    db.update_dal(rset_dal)
+                    daqcon_dal = dal.DetectorToDaqConnection(
+                        f"{last_eth_pars['rx_host']}-connections",
+                        contains = [rset_dal, dpdkrec_dal]
                     )
-                    db.update_dal(rogroup_dal)
-                    groups.append(rogroup_dal)
+                    db.update_dal(daqcon_dal)
+
+                    rset_dal = dal.ResourceSetAND(
+                        f"{last_eth_pars['rx_host']}-streams",
+                        contains = eth_streams
+                    )
+                    db.update_dal(rset_dal)
+
                     eth_streams = []
+                    hermes_streams = []
+                    eth_senders = []
                     rx_queue = 0
+            # Update Hermes ids now _after_ making any DataSenders or
+            # Controllers since we don't make them for the current
+            # params but the ones from the loop before
+            hermes_id = f"hermes_{geo_id['det_id']}_{geo_id['crate_id']}_{geo_id['slot_id']}"
+            hermes_link_id = f"{hermes_id}-{link_number}"
             if pars != last_eth_pars:
-                # Only create a new dal object if the parameters are different to the last one
-                stream_pars = dal.EthStreamParameters(
-                    f"pars-{source_id}",
-                    protocol = pars["protocol"],
-                    mode = pars["mode"],
-                    tx_hostname = pars["tx_host"],
-                    tx_mac = pars["tx_mac"],
-                    tx_ip = pars["tx_ip"],
-                    lcore = lcores[rx_queue%len(lcores)],
-                    rx_queue = rx_queue
-                )
-                db.update_dal(stream_pars)
                 rx_queue = rx_queue + 1
                 last_eth_pars = pars
                 last_eth_source_id = source_id
+                last_tx_mac = pars["tx_mac"]
+                last_tx_host = pars["tx_host"]
+
+
         elif entry["kind"] == "flx":
+            print (f"Processing config for FELIX {source_id=}")
             flx_source_id = source_id
             flx_streams_found = True
             pars = entry["parameters"]
@@ -114,132 +190,99 @@ def dro_json_to_oks(jsonfile, oksfile, source_id_offset, nomap, lcores):
                     felix_dal = dal.FelixInterface(
                         f"felix-{last_felix_source_id}",
                         card=last_felix_pars["card"],
-                        slr=last_felix_pars["slr"],
-                        contains=flx_streams
+                        slr=last_felix_pars["slr"]
                     )
                     db.update_dal(felix_dal)
-                    rogroup_dal = dal.ReadoutGroup(
-                        f"group-{last_felix_source_id}",
-                        contains=[felix_dal]
+                    rset_dal = dal.ResourceSetAND(
+                        f"felix-{last_source_id}-streams",
+                        contains = flx_streams
                     )
-                    db.update_dal(rogroup_dal)
-                    groups.append(rogroup_dal)
+                    db.update_dal(rset_dal)
+                    daqcon_dal = dal.DetectorToDaqConnection(
+                        f"felix-{last_source_id}-connections",
+                        contains = [rset_dal, felix_dal]
+                    )
+                    db.update_dal(daqcon_dal)
                     flx_streams = []
-            stream_pars = dal.FelixStreamParameters(
-                f"flxpars-{source_id}",
+            # Not sure how FelixDataSender fits in. What uses it?
+            flx_sender_dal = dal.FelixDataSender(
+                f"flxsender-{source_id}",
                 protocol=pars["protocol"],
-                mode=pars["mode"],
-                link=pars["link"]
+                link=pars["link"],
+                contains = [stream_dal]
             )
-            db.update_dal(stream_pars)
+            db.update_dal(flx_sender_dal)
+            flx_senders.append(flx_sender_dal)
             last_felix_pars = pars
             last_felix_source_id = source_id
         else:
             raise RuntimeError(f'Unknown kind of readout {entry["kind"]}!')
 
-        stream = dal.DROStreamConf(
-            f"DROStream-{source_id}",
-            source_id=entry["src_id"],
-            stream_params=stream_pars,
-            geo_id=geo_dal
-        )
-        db.update_dal(stream)
         if entry["kind"] == "eth":
-            eth_streams.append(stream)
+            eth_streams.append(stream_dal)
+            hermes_streams.append(stream_dal)
         else:
-            flx_streams.append(stream)
+            flx_streams.append(stream_dal)
+
         last_source_id = source_id
-        stream_dals[source_id] = stream
 
     if eth_streams_found:
-        nic_name = f"nic-{last_eth_pars['rx_host']}"
-        print(
-            f"Ending by adding nic {last_eth_pars['rx_mac']} with id {nic_name}"
-        )
-        nic_dal = dal.NICInterface(
-            nic_name,
-            rx_hostname=last_eth_pars["rx_host"],
-            rx_mac=last_eth_pars["rx_mac"],
-            rx_ip=last_eth_pars["rx_ip"],
-            rx_iface=last_eth_pars["rx_iface"],
-            rx_pcie_addr=last_eth_pars["rx_pcie_dev"],
-            contains=eth_streams,
-            configuration=nic_config_dal
-        )
-        nic_dals[nic_name] = nic_dal
-        db.update_dal(nic_dal)
-        rogroup_dal = dal.ReadoutGroup(f"group-{eth_source_id}", contains=[nic_dal])
-        db.update_dal(rogroup_dal)
-        groups.append(rogroup_dal)
-
-        address_table_dal = dal.IpbusAddressTable("Hermes-addrtab")
-        db.update_dal(address_table_dal)
-        # Loop over the json again to generate the Hermes links and controllers
-        last_pars = None
-        link_number = 0
-        links = []
-        last_tx_mac = None
-        for entry in jsonmap:
-            if entry["kind"] == "eth":
-                pars = entry["parameters"]
-                if last_pars != None:
-                    if pars["tx_host"] != last_pars['tx_host']:
-                        # print(f"Adding HermesController {hermes_id} for {pars['tx_host']=} {last_pars['tx_host']=}")
-                        hermes_controller_dal = dal.HermesController(
-                            hermes_id,
-                            uri=f"ipbusudp-2.0://{last_pars['tx_host']}:50001",
-                            address_table=address_table_dal,
-                            links=links
-                        )
-                        db.update_dal(hermes_controller_dal)
-                        links = []
-                        link_number = 0
-
-                geo_id = entry["geo_id"]
-                hermes_id = f"hermes_{geo_id['det_id']}_{geo_id['crate_id']}_{geo_id['slot_id']}"
-                source_id = entry["src_id"] + source_id_offset
-                nic_name = f"nic-{pars['rx_host']}"
-                hermes_link_id = f"{hermes_id}-{link_number}"
-                if pars["tx_mac"] != last_tx_mac:
-                    link_dal = dal.HermesLinkConf(
-                        hermes_link_id,
-                        link_id=link_number,
-                        source=stream_dals[source_id],
-                        destination=nic_dals[nic_name]
-                    )
-                    db.update_dal(link_dal)
-                    links.append(link_dal)
-                    link_number = link_number + 1
-                last_pars = pars
-                last_tx_mac = pars["tx_mac"]
-                last_tx_host = pars["tx_host"]
         if link_number > 0:
-            # print(f"Adding HermesController {hermes_id}")
+            print(f"Adding final HermesDataSender {hermes_link_id}")
+            if len(eth_streams) > 0:
+                link_dal = dal.HermesDataSender(
+                    hermes_link_id,
+                    link_id = link_number,
+                    contains = hermes_streams,
+                    uses = txnic_dal
+                )
+                db.update_dal(link_dal)
+                links.append(link_dal)
+            print(f"Adding final HermesController {hermes_id}")
             hermes_controller_dal = dal.HermesController(
                 hermes_id,
-                uri=f"ipbusudp-2.0://{last_pars['tx_host']}:50001",
-                address_table=address_table_dal,
-                links=links
+                uri = f"ipbusudp-2.0://{last_eth_pars['tx_host']}:50001",
+                address_table = address_table_dal,
+                links = links,
+                destination = rxnic_dal
             )
             db.update_dal(hermes_controller_dal)
 
 
-    if flx_streams_found and len(flx_streams) > 0:
+        rset_dal = dal.ResourceSetAND(
+            f"{last_eth_pars['rx_host']}-senders",
+            contains = eth_senders
+        )
+        db.update_dal(rset_dal)
+        rset_dal = dal.ResourceSetAND(
+            f"{last_eth_pars['rx_host']}-streams",
+            contains = eth_streams
+        )
+        db.update_dal(rset_dal)
+        daqcon_dal = dal.DetectorToDaqConnection(
+            f"{last_eth_pars['rx_host']}-connections",
+            contains = [rset_dal, dpdkrec_dal]
+        )
+        db.update_dal(daqcon_dal)
+
+
+    if flx_streams_found and len(flx_senders) > 0:
         print(f"Adding final FelixInterface felix-{flx_source_id}")
         felix_dal = dal.FelixInterface(
             f"felix-{flx_source_id}",
             card=last_felix_pars["card"],
-            slr=last_felix_pars["slr"],
-            contains=flx_streams
+            slr=last_felix_pars["slr"]
         )
         db.update_dal(felix_dal)
-        rogroup_dal = dal.ReadoutGroup(f"group-{flx_source_id}", contains=[felix_dal])
-        db.update_dal(rogroup_dal)
-        groups.append(rogroup_dal)
-
-    if not nomap:
-        map_dal = dal.ReadoutMap("readoutmap", groups=groups)
-        db.update_dal(map_dal)
-
+        rset_dal = dal.ResourceSetAND(
+            f"felix-{flx_source_id}-streams",
+            contains = flx_streams
+        )
+        db.update_dal(rset_dal)
+        daqcon_dal = dal.DetectorToDaqConnection(
+            f"felix-{flx_source_id}-connections",
+            contains = [rset_dal, felix_dal]
+        )
+        db.update_dal(daqcon_dal)
 
     db.commit()
