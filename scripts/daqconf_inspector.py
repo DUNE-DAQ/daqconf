5150dded7bafb8eb3fb791914d6a4b8f297d0bab
Alessandro Thea <alessandro.thea@cern.ch>
2024-08-31 08:23:11 +0200

    Few more updates

--- scripts/daqconf_inspector.py
+++ scripts/daqconf_inspector.py
@@ -19,6 +19,7 @@ def make_segment_tree(cfg, segment, session: None, show_path: bool = False) -> T
     Create segment branch of the configuration tree
     '''
 
+
     def enabled_to_emoji(enabled: Union[int, None]) -> str:
         match enabled:
             case 1:
@@ -28,9 +29,12 @@ def make_segment_tree(cfg, segment, session: None, show_path: bool = False) -> T
             case -1:
                 return ':x:' 
             case _:
-                return ''
+                return ':blue_circle:'
             
     def get_enabled(cfg, session, obj):
+        if not obj.isDalType('Component'):
+            return None
+
         enabled = not confmodel.component_disabled(cfg._obj, session.id, obj.id)
         if enabled:
             return enabled
@@ -38,9 +42,6 @@ def make_segment_tree(cfg, segment, session: None, show_path: bool = False) -> T
         enabled -= (obj in session.disabled)
         return enabled
         
-
-
-
 
     path = f"[blue]{cfg.get_obj(segment.className(), segment.id).contained_in()}[/blue]" if show_path else ""
     enabled = get_enabled(cfg, session, segment) if session else None
@@ -95,6 +96,7 @@ def cli(obj, interactive, config_file):
 @click.pass_obj
 def show_sessions(obj, show_paths):
     """
+    Display the list of sessions available in the configuration database and their internal structure
     """
     
     cfg = obj.cfg
@@ -114,12 +116,31 @@ def show_sessions(obj, show_paths):
         print(tree)
     print()
 
+
+@cli.command()
+@click.pass_obj
+def list_classes(obj):
+    """Prints the list of classes known to configuration"""
+    from rich.highlighter import ReprHighlighter
+
+    rh = ReprHighlighter()
+    cfg = obj.cfg
+
+    table = Table("Classes")
+    table.add_column('Class', 'Objects')
+    for k in sorted(cfg.classes()):
+        table.add_row(k, rh(str([o.UID() for o in cfg.get_objs(k)])))
+    print(table)
+
 @cli.command()
 @click.argument('klass')
 @click.option('-v/-h','--vertical/--horizontal', "vtable", default=True)
 @click.pass_obj
 def show_class(obj, klass,vtable):
+    from rich.highlighter import ReprHighlighter
 
+    rh = ReprHighlighter()
+ 
     cfg = obj.cfg
     
     if klass not in cfg.classes():
@@ -140,16 +161,16 @@ def show_class(obj, klass,vtable):
             table.add_column(do.id)
 
         for a in attrs:
-            table.add_row(*([a]+[str(getattr(do,a)) for do in dals]))
+            table.add_row(*([a]+[rh(str(getattr(do,a))) for do in dals]))
 
         for r in rels:
             rel_vals = [getattr(do,r) for do in dals]
             rel_strs = []
             for rv in rel_vals:
                 if isinstance(rv,list):
-                    rel_strs += [','.join([getattr(v,'id', 'None') for v in rv])]
+                    rel_strs += [rh(str([getattr(v,'id', 'None') for v in rv]))]
                 else:
-                    rel_strs += [getattr(rv,'id', 'None')]
+                    rel_strs += [rh(getattr(rv,'id', 'None'))]
             table.add_row(*([f"{r} ([yellow]{rels[r]['type']}[/yellow])"]+rel_strs))
     else:
 
@@ -162,14 +183,14 @@ def show_class(obj, klass,vtable):
             table.add_column(f"{r} ([yellow]{rels[r]['type']}[/yellow])")
         
         for do in dals:
-            attr_vals = [str(getattr(do,a)) for a in attrs]
+            attr_vals = [rh(str(getattr(do,a))) for a in attrs]
             rel_vals = [getattr(do,r) for r in rels]
             rel_strs = []
             for rv in rel_vals:
                 if isinstance(rv,list):
-                    rel_strs += [','.join([getattr(v,'id', 'None') for v in rv])]
+                    rel_strs += [rh(str([getattr(v,'id', 'None') for v in rv]))]
                 else:
-                    rel_strs += [getattr(rv,'id', 'None')]
+                    rel_strs += [rh(getattr(rv,'id', 'None'))]
             table.add_row(*([do.id]+attr_vals+rel_strs))
 
     print(table)
@@ -179,7 +200,8 @@ def show_class(obj, klass,vtable):
 @click.argument('id')
 @click.pass_obj
 def show_object_tree(obj, klass, id):
-
+    from rich.highlighter import ReprHighlighter
+    rh = ReprHighlighter()
     cfg = obj.cfg
 
     if klass not in cfg.classes():
@@ -187,29 +209,13 @@ def show_object_tree(obj, klass, id):
         print(f'Known classes: {sorted(cfg.classes())}')
         raise SystemExit(-1)
 
-    do = cfg.get_dal(klass, id)
-
-    def make_obj_tree(dal_obj):
-        tree = Tree(f"[green]{dal_obj.id}[/green][magenta]@{dal_obj.className()}[/magenta]")
-        attr_tree = tree.add('[yellow]attributes[/yellow]')
-        attrs = cfg.attributes(dal_obj.className(), True)
-        for a in attrs:
-            attr_tree.add(f"[cyan]{a}[/cyan] = {getattr(dal_obj, a)}")
-        
-        rel_tree = tree.add('[yellow]relationships[/yellow]')
-        rels = cfg.relations(dal_obj.className(), True)
-        for rel, rinfo in rels.items():
-            rel_val = getattr(dal_obj, rel)
-            r_tree = rel_tree.add(f"[cyan]{rel}[/cyan]@[magenta]{rinfo['type']}[/magenta] {('['+str(len(rel_val))+']' if isinstance(rel_val, list) else '')}")
-            if not isinstance(rel_val,list):
-                rel_val = [rel_val]
-            for v in rel_val:
-                if v is None:
-                    continue
-                r_tree.add(make_obj_tree(v))
-        return tree
     
+    tokens = id.split('.')
+    parent = tokens[0]
+    children = tokens[1:]
 
+    do = cfg.get_dal(klass, id)
+    
     def make_obj_tree(dal_obj):
         tree = Tree(f"[green]{dal_obj.id}[/green][magenta]@{dal_obj.className()}[/magenta]")
         # attr_tree = tree.add('[yellow]attributes[/yellow]')
@@ -231,13 +237,8 @@ def show_object_tree(obj, klass, id):
         return tree
 
 
-    tree = make_obj_tree(do)
+    tree = make_obj_tree(do, children)
     print(tree)
-
-
-    if False:
-        import IPython
-        IPython.embed(colors='neutral')
 
 
 @cli.command()
