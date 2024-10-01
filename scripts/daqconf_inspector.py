41ad32e52532b143bf7f938efa1ffaea1a454057
Alessandro Thea <alessandro.thea@cern.ch>
2024-10-01 19:35:44 +0200

    Adding inspector documentation

--- scripts/daqconf_inspector.py
+++ scripts/daqconf_inspector.py
@@ -7,49 +7,133 @@ from rich.table import Table
 import click
 
 import conffwk
-import confmodel
 
 import IPython
 
-class DaqInspectorContext:
-    pass
+
+def is_enabled(cfg, session, obj):
+    import confmodel
+
+    """Helper function that returns the status of an object in a session"""
+    if not obj.isDalType('Component'):
+        return None
+
+    enabled = not confmodel.component_disabled(cfg._obj, session.id, obj.id)
+    if enabled:
+        return enabled
+    
+    enabled -= (obj in session.disabled)
+    return enabled
+
+def get_attribute_info(o):
+    return o.__schema__['attribute']
+
+def get_relation_info(o):
+    return o.__schema__['relation']
+
+def get_attribute_list(o):
+    return list(get_attribute_info(o))
+
+def get_relation_list(o):
+    return list(get_relation_info(o))
+
+def get_superclass_list(o):
+    return o.__schema__['superclass']
+
+def get_subclass_list(o):
+    return o.__schema__['subclass']
+
+def compare_dal_obj(a, b):
+    """Compare two dal objects by content"""
+
+    # TODO: add a check on a and b being dal objects
+    # There is no base class for dal objects in python, but dal objects have _shcema__objects.
+
+    if a.className() != b.className():
+        return False
+
+    attrs = get_attribute_list(a)
+    rels = get_relation_list(a)
+
+    a_attrs = {x:getattr(a, x) for x in attrs}
+    b_attrs = {x:getattr(b, x) for x in attrs}
+
+    a_rels = {x:getattr(a, x) for x in rels}
+    b_rels = {x:getattr(b, x) for x in rels}
+
+
+    return (a_attrs == b_attrs) and (a_rels == b_rels)
+
+
+#---------------
+def find_related_dals(dal_obj, dal_group: set):
+
+
+    rels = get_relation_list(dal_obj)
+
+    rel_objs = set()
+    for rel in rels:
+        rel_val = getattr(dal_obj, rel)
+
+        if rel_val is None:
+            continue
+
+        rel_objs.update(rel_val if isinstance(rel_val,list) else [rel_val])
+
+    # Isolate relationship objects that are not in the dal_group yet
+    new_rel_objs = rel_objs - dal_group
+
+    # Safely add the new object to the group
+    dal_group.update(rel_objs)
+    for o in new_rel_objs:
+        if o is None:
+            continue
+
+        find_related_dals(o, dal_group)
+        
+from collections.abc import Iterable
+def find_duplicates( collection: Iterable ):
+    """
+    Find duplicated dal objects in a collection by based on objects attributes and relationships
+    """
+    
+    n_items = len(collection)
+    duplicates = set()
+    for i in range(n_items):
+        for j in range(i+1, n_items):
+            if compare_dal_obj(collection[i], collection[j]):
+                print(f"{i} {j} AAAAARGH")
+                duplicates.add(collection[i])
+                duplicates.add(collection[j])
+
+    return duplicates
+
+def enabled_to_emoji(enabled: Union[int, None]) -> str:
+    """
+    Convert enabled values [enabled, disabled, disabled-by-logic] into standard emojis
+    """
+    match enabled:
+        case 1:
+            return ':white_check_mark:'
+        case 0:
+            return ':heavy_large_circle:' 
+        case -1:
+            return ':x:' 
+        case _:
+            return ':blue_circle:'
 
 def make_segment_tree(cfg, segment, session: None, show_path: bool = False) -> Tree:
     '''
-    Create segment branch of the configuration tree
-    '''
-
-
-    def enabled_to_emoji(enabled: Union[int, None]) -> str:
-        match enabled:
-            case 1:
-                return ':white_check_mark:'
-            case 0:
-                return ':heavy_large_circle:' 
-            case -1:
-                return ':x:' 
-            case _:
-                return ':blue_circle:'
-            
-    def get_enabled(cfg, session, obj):
-        if not obj.isDalType('Component'):
-            return None
-
-        enabled = not confmodel.component_disabled(cfg._obj, session.id, obj.id)
-        if enabled:
-            return enabled
-        
-        enabled -= (obj in session.disabled)
-        return enabled
-        
+    Create a segment branch of the session tree as a rich.Tree object
+    ''' 
 
     path = f"[blue]{cfg.get_obj(segment.className(), segment.id).contained_in()}[/blue]" if show_path else ""
-    enabled = get_enabled(cfg, session, segment) if session else None
+    enabled = is_enabled(cfg, session, segment) if session else None
     tree = Tree(f"{enabled_to_emoji(enabled)} [yellow]{segment.id}[/yellow] {path}")
 
     c = segment.controller
     path = f"[blue]{cfg.get_obj(c.className(), c.id).contained_in()}[/blue]" if show_path else ""
-    ports = ', '.join([f'{svc.port}({svc.protocol})' for svc in c.exposes_service ])
+    ports = ', '.join([f'{svc.port}([orange1]{svc.protocol}[/orange1])' for svc in c.exposes_service ])
     host = f"[medium_purple1]{c.runs_on.runs_on.id}[/medium_purple1]"
     tree.add(f"[cyan]controller[/cyan]: [green]{c.id}[/green][magenta]@{c.className()}[/magenta] on {host} [{ports}] {path}")
 
@@ -61,9 +145,9 @@ def make_segment_tree(cfg, segment, session: None, show_path: bool = False) -> T
                 continue
 
             path = f"[blue]{cfg.get_obj(a.className(), a.id).contained_in()}[/blue]" if show_path else ""
-            ports = ', '.join([f'{svc.port}({svc.protocol})' for svc in c.exposes_service ])
+            ports = ', '.join([f'{svc.port}([orange1]{svc.protocol}[/orange1])' for svc in c.exposes_service ])
             host = f"[medium_purple1]{a.runs_on.runs_on.id}[/medium_purple1]"
-            enabled = get_enabled(cfg, session, a) if session else None
+            enabled = is_enabled(cfg, session, a) if session else None
 
             app_tree.add(f"{enabled_to_emoji(enabled)} [green]{a.id}[/green][magenta]@{a.className()}[/magenta] on {host} [{ports}] {path}")
 
@@ -76,14 +160,34 @@ def make_segment_tree(cfg, segment, session: None, show_path: bool = False) -> T
             seg_tree.add(make_segment_tree(cfg, s, session, show_path))
 
     return tree
-    
 
 
-@click.group()
-@click.option('-i', '--interactive', is_flag=True, show_default=True, default=False)
+def validate_oks_uid(ctx, param, value):
+    """
+    Helper function to validate OKS UID options or arguments accorinf to the format id@class
+    """
+    if isinstance(value, tuple):
+        return value
+
+    try:
+        id, _, klass = value.partition("@")
+        return str(id), str(klass)
+    except ValueError:
+        raise click.BadParameter("format must be '<id>@<class>'")
+
+class DaqInspectorContext:
+    pass
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.option('-i', '--interactive', is_flag=True, show_default=True, default=False, help="Start an interactive IPython session after executing the commands")
 @click.argument('config_file')
 @click.pass_obj
 def cli(obj, interactive, config_file):
+    """
+    A collection of helper commands to inspect dunedaq OKS databases and objects
+    """
     cfg = conffwk.Configuration(f"oksconflibs:{config_file}")
     obj.cfg = cfg
 
@@ -91,13 +195,15 @@ def cli(obj, interactive, config_file):
         IPython.embed(colors="neutral")
 
 
-@cli.command()
-@click.option('-p', '--show-paths', is_flag=True, show_default=True, default=False)
+@cli.command(short_help="Show sessions information")
+@click.option('-p', '--show-file_paths', is_flag=True, show_default=True, default=False)
 @click.pass_obj
-def show_sessions(obj, show_paths):
+def show_sessions(obj, show_file_paths):
     """
-    Display the list of sessions available in the configuration database and their internal structure
+    Show high level information about the sessions from the configuration database
     """
+    from rich.highlighter import ReprHighlighter
+    rh = ReprHighlighter()
     
     cfg = obj.cfg
 
@@ -109,18 +215,81 @@ def show_sessions(obj, show_paths):
     print()
 
     for so in sessions:
+
+        grid = Table.grid("")
+        
+
         s = cfg.get_dal('Session', so.UID())
-        tree = Tree(f"Session [yellow]{s.id}[/yellow]")
+        tree = Tree(f"[yellow]{s.id}[/yellow]")
+        infra_tree = tree.add('[yellow]infra-apps[/yellow]')
+        for a in s.infrastructure_applications:
+            infra_tree.add(a.id)
 
-        tree.add(make_segment_tree(cfg, s.segment, s, show_paths))
-        print(tree)
-    print()
+        tree.add(make_segment_tree(cfg, s.segment, s, show_file_paths))
+
+        t = Table("body", title='Session Tree', show_header=False, expand=True)
+        t.add_row(tree)
+        grid.add_row(t)
+        
+        # print(t)
+        
+        session_objs = set()
+        find_related_dals(s, session_objs)
+
+        # Find all objects in the top segment (exclude disabled, variables and infra in the resource count))
+        segment_objs = set()
+        find_related_dals(s.segment, segment_objs)        
+        res = [o for o in segment_objs if 'ResourceBase' in o.oksTypes()]
 
 
-@cli.command()
+
+        t = Table(title=s.id, show_header=False, expand=True)
+        t.add_column('name')
+        t.add_column('value')
+
+        t.add_row('Objects', rh(str(len(session_objs))))
+        t.add_row('Disabled mask', rh(str([ i.id for i in s.disabled])))
+        t.add_row('Disabled resources', rh(str([ r.id for r in res if is_enabled(cfg, s, r) != 1])))
+        # print(t)
+        grid.add_row(t)
+
+        disabled_not_in_session = set(s.disabled)-segment_objs
+        if disabled_not_in_session:
+            print(f"⚠️ {sorted(disabled_not_in_session)}")
+
+        if s.environment:
+            # print(s.environment)
+            env_objs = set()
+            for e in s.environment:
+                env_objs.add(e)
+                find_related_dals(e, env_objs)
+            # print(env_objs)
+            # continue
+
+            env_var = sorted(env_objs, key=lambda x: x.id)
+
+            t = Table(title="Environment", show_header=False,  expand=True)
+            t.add_column('name')
+            t.add_column('value')
+            for e in env_var:
+                if 'Variable' not in e.oksTypes():
+                    continue
+                t.add_row(e.id, e.value)
+            # print(t)
+            grid.add_row(t)
+            print(grid)
+            print()
+
+    # IPython.embed(colors="neutral")
+
+@cli.command(short_help="List known classes and objects for each class")
 @click.pass_obj
-def list_classes(obj):
-    """Prints the list of classes known to configuration"""
+@click.option('-d', "--show-derived-objects-as-parents", "show_derived", is_flag=True, default=False, help="Include derived objects in parent class listing")
+def list_classes(obj, show_derived):
+    """
+    Prints on screen the list of classes known to the schema,
+    together with the ids of objects belonging to that class.
+    """
     from rich.highlighter import ReprHighlighter
 
     rh = ReprHighlighter()
@@ -129,14 +298,18 @@ def list_classes(obj):
     table = Table("Classes")
     table.add_column('Class', 'Objects')
     for k in sorted(cfg.classes()):
-        table.add_row(k, rh(str([o.UID() for o in cfg.get_objs(k)])))
+        table.add_row(k, rh(str([o.UID() for o in cfg.get_objs(k) if (o.class_name() == k or show_derived)])))
     print(table)
 
-@cli.command()
+@cli.command(short_help="Show properties of objects forma class")
 @click.argument('klass')
-@click.option('-v/-h','--vertical/--horizontal', "vtable", default=True)
+@click.option('-v/-h','--vertical/--horizontal', "vtable", default=True, help="Vertical or horizontal orientation")
 @click.pass_obj
-def show_class(obj, klass,vtable):
+def show_obj_of_class(obj, klass, vtable):
+    """
+    Show attributes and relationships of all objects in the database belonging to KLASS
+    """
+
     from rich.highlighter import ReprHighlighter
 
     rh = ReprHighlighter()
@@ -195,11 +368,16 @@ def show_class(obj, klass,vtable):
 
     print(table)
 
-@cli.command()
+@cli.command(short_help="Show relationship tree")
 @click.argument('klass')
 @click.argument('id')
+@click.option('+a/-a','--show-attributes/--hide-attributes', "show_attrs", default=True, help="Show/Hide attributes")
+@click.option('-l','--level', "level", default=True, help="Recursion level in the object tree")
 @click.pass_obj
-def show_object_tree(obj, klass, id):
+def show_relation_tree(obj, klass, id, show_attrs):
+    """
+    Show the relationship tree of the OKS object with ID@KLASS identifier.
+    """
     from rich.highlighter import ReprHighlighter
     rh = ReprHighlighter()
     cfg = obj.cfg
@@ -210,43 +388,158 @@ def show_object_tree(obj, klass, id):
         raise SystemExit(-1)
 
     
-    tokens = id.split('.')
-    parent = tokens[0]
-    children = tokens[1:]
+    path = id.split('.')
+    parent = path[0]
 
-    do = cfg.get_dal(klass, id)
-    
-    def make_obj_tree(dal_obj):
+    try:
+        do = cfg.get_dal(klass, parent)
+    except RuntimeError as e:
+        raise click.BadArgumentUsage(f"Object '{id}' does not exist")
+
+
+
+    def make_obj_tree(dal_obj, show_attrs, path=[]):
+
         tree = Tree(f"[green]{dal_obj.id}[/green][magenta]@{dal_obj.className()}[/magenta]")
-        # attr_tree = tree.add('[yellow]attributes[/yellow]')
+
         attrs = cfg.attributes(dal_obj.className(), True)
-        for a in attrs:
-            tree.add(f"[cyan]{a}[/cyan] = {getattr(dal_obj, a)}")
-        
-        # rel_tree = tree.add('[yellow]relationships[/yellow]')
+        rels = cfg.relations(dal_obj.className(), True)
+
+        rel_sel = None
+        if path:
+            if path[0] not in rels:
+                raise click.BadArgumentUsage(f"Object '{path[0]}' does not exist in {dal_obj.id}")
+            else:
+                rel_sel = path[0]
+                path = path[1:]
+
+        if show_attrs:
+            for a in attrs:
+                tree.add(f"[cyan]{a}[/cyan] = {getattr(dal_obj, a)}")
+            
         rels = cfg.relations(dal_obj.className(), True)
         for rel, rinfo in rels.items():
+            # Filter on relationship name if path is specified
+            if rel_sel and rel != rel_sel:
+                continue
+
             rel_val = getattr(dal_obj, rel)
             r_tree = tree.add(f"[yellow]{rel}[/yellow]@[magenta]{rinfo['type']}[/magenta] {('['+str(len(rel_val))+']' if isinstance(rel_val, list) else '')}")
+
             if not isinstance(rel_val,list):
                 rel_val = [rel_val]
-            for v in rel_val:
-                if v is None:
+
+            for val in rel_val:
+                if path and val.id != path[0]:
                     continue
-                r_tree.add(make_obj_tree(v))
+                if val is None:
+                    continue
+                r_tree.add(make_obj_tree(val, show_attrs, path[1:]))
         return tree
 
 
-    tree = make_obj_tree(do, children)
+    tree = make_obj_tree(do, show_attrs, path[1:])
     print(tree)
+
+        
+@cli.command()
+@click.argument('uid', type=click.UNPROCESSED, callback=validate_oks_uid, default=None)
+@click.pass_obj
+def test_find_relations(obj, uid):
+
+    cfg = obj.cfg
+    id, klass = uid
+
+
+    if klass not in cfg.classes():
+        print(f'[red]Class {klass} unknow to configuration[/red]')
+        print(f'Known classes: {sorted(cfg.classes())}')
+        raise SystemExit(-1)
+
+    try:
+        dal_obj = cfg.get_dal(klass, id)
+    except RuntimeError as e:
+        raise click.BadArgumentUsage(f"Object '{id}' does not exist")
+    
+    print(dal_obj)
+    
+    grp = set()
+    find_related_dals(dal_obj, grp)
+
+    print(f"Found {len(grp)} objects related to {id}@{klass}")
+    # print(grp)
+    IPython.embed(colors="neutral")
 
 
 @cli.command()
 @click.pass_obj
 def check_detstreams(obj):
 
+    from rich.highlighter import ReprHighlighter
+    rh = ReprHighlighter()
     cfg = obj.cfg
-    print("DetectorStreams")
+
+
+    klass = 'DetectorStream'
+    ds_attrs = cfg.attributes(klass, True)
+    ds_rels = cfg.relations(klass, True)
+
+    if list(ds_rels) != ['geo_id']:
+        raise click.ClickException(f"Unexpected relationships found in DetectorStream {ds_rels}")
+
+
+    gid_attrs = cfg.attributes("GeoId", True)
+    gid_rels = cfg.relations("GeoId", True)
+
+    if gid_rels:
+        raise click.ClickException("Geoids are not expected to have relationships")
+
+
+    streams = cfg.get_dals(klass)
+    streams = sorted(streams, key=lambda x: x.source_id)
+
+
+    table = Table(title='DetectorStreams')
+    table.add_column('id')
+    table.add_column('status')
+
+    for a in ds_attrs:
+        table.add_column(a)
+
+    table.add_column('geo_id.id')
+
+    for a in gid_attrs:
+        table.add_column(f"geoid.{a}")
+
+
+    for strm in streams:
+        enabled_marker = ':blue_circle:'
+        
+        ds_attr_vals = [rh(str(getattr(strm,a))) for a in ds_attrs]
+        gid_attr_vals = [rh(str(getattr(strm.geo_id,a))) for a in gid_attrs]
+
+        table.add_row(*([strm.id, enabled_marker]+ds_attr_vals+[strm.geo_id.id]+gid_attr_vals))
+
+    print(table)
+
+    # Check for duplicate strm
+    strm_duplicates = find_duplicates(streams)
+
+    if strm_duplicates:
+        print(f"[yellow]WARNING: found duplicates in detector streams {strm_duplicates}[/yellow]")
+    else:
+        print(f"[green]:white_check_mark: No duplicates among detector streams[/green]")
+
+
+    gid_duplicates = find_duplicates([strm.geo_id for strm in streams])
+
+    if gid_duplicates:
+        print(f"[yellow]WARNING: found duplicates in geo ids {strm_duplicates}[/yellow]")
+    else:
+        print(f"[green]:white_check_mark: No duplicates among geo ids[/green]")
+
+
+
 
     IPython.embed(colors="neutral")
 
