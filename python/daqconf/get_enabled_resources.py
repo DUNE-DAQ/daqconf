912c759c40d5e668d0aeb19b7c39623d0e3824d8
Henry Wallace <henry.wallace@rhul.ac.uk>
2026-02-02 14:48:16 +0000

    add resource collector

---
+++ python/daqconf/get_enabled_resources.py
@@ -0,0 +1,19 @@
+from conffwk import Configuration
+
+def get_enabled_resources(oksfile: str, session_name: str, resource_class="Resource"):
+    """Function to list enabled resources in the specified OKS database file"""
+    
+    db = Configuration("oksconflibs:" + oksfile)  
+    
+    session = db.get_session(session_name)
+    if not session:
+        raise ValueError(f"Session '{session_name}' not found in database '{oksfile}'")
+
+    enabled_resources = []
+    enabled_resource_dals = []
+    for resource in session.get_dals(resource_class):
+        if resource.is_enabled():
+            enabled_resources.append([getattr(resource, "id"), getattr(resource, "className")()])
+            enabled_resource_dals.append(resource)
+    
+    return enabled_resources, enabled_resource_dals
