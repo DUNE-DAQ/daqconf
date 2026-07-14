f7bc14a2ef739c252b7201e748d0deea638e114e
Kurt Biery <biery@daq.fnal.gov>
2026-07-14 14:04:32 -0500

    Added 'quiet' flag to get_session_env_var to avoid messages in certain cases.

--- python/daqconf/get_session_env_var.py
+++ python/daqconf/get_session_env_var.py
@@ -2,18 +2,20 @@ import conffwk
 import confmodel_dal
 import sys
 
-def get_session_env_var(oksfile, session_name, requested_env_var_name):
+def get_session_env_var(oksfile, session_name, requested_env_var_name, quiet=False):
     """Script to get the value of an environment variable in the specified Session of the
     specified OKS database file"""
     db = conffwk.Configuration("oksconflibs:" + oksfile)
     if session_name == "":
-        print(f"Error: the session name needs to be specified", file=sys.stderr)
+        if not quiet:
+            print(f"Error: the session name needs to be specified", file=sys.stderr)
         return
     else:
         try:
             session = db.get_dal("Session", session_name)
         except:
-            print(f"Error: could not find Session \"{session_name}\" in file \"{oksfile}\"", file=sys.stderr)
+            if not quiet:
+                print(f"Error: could not find Session \"{session_name}\" in file \"{oksfile}\"", file=sys.stderr)
             return
 
     schemafiles = [
@@ -38,7 +40,8 @@ def get_session_env_var(oksfile, session_name, requested_env_var_name):
 
     # Return the value, or complain and return None if the env var was not found
     if existing_env_var is None:
-        print(f"Error: could not find env var \"{requested_env_var_name}\" in Session \"{session_name}\"", file=sys.stderr)
+        if not quiet:
+            print(f"Error: could not find env var \"{requested_env_var_name}\" in Session \"{session_name}\"", file=sys.stderr)
         return
     else:
         value = existing_env_var.value
